c96611a4299f8dfc986809e0d8c4415e30f49dbc
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-09-12 19:15:59 -0500

    Code includes routes

--- app/app.js
+++ app/app.js
@@ -8,10 +8,20 @@ var App;
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
+  LOG_TRANSITIONS: true,
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
+
+App.Router.map(function(){
+  this.route('about', {path: '/aboutus'});
+});
+
+// The about path will be loaded in the 'outlet section'
+// The index.html loads first, then goes to retreive each of the templates
+// #tells the browser that everything after is not part of the file template
+
 
 loadInitializers(App, config.modulePrefix);
 
